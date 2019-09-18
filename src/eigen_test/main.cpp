01a757edbd40e585bdce6e827d482b0728f9001b
spidersss <604815982@qq.com>
2019-09-18 19:59:37 +0800

    ..

---
+++ src/eigen_test/main.cpp
@@ -0,0 +1,33 @@
+#include<Eigen/Dense>
+#include<iostream>
+#include<deque>
+
+using namespace std;
+
+int main()
+{
+	Eigen::Matrix3d information_matrix = Eigen::Matrix3d::Identity();
+	information_matrix.block<2, 2>(1, 1) /= 5;
+	
+	cout << information_matrix << endl;
+	
+	std::deque<int> num_deque(10);
+	int i=0;
+	for(auto& num:num_deque)
+		num = i++;
+	
+	for(auto& num:num_deque)
+		cout << hex << &num  <<": "<<  dec << num<< endl;
+	cout << endl;	
+	
+	num_deque.erase(num_deque.begin(),num_deque.end()-2);
+	num_deque.push_back(11);
+	
+	for(auto& num:num_deque)
+		cout << hex << &num  <<": "<<  dec << num<< endl;
+		
+		
+	
+	
+	return 0;
+}
