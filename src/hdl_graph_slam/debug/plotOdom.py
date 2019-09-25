94e2e4744c8848cf38b9ac9fedf83dd52e50d886
CastielLiu <castiel_liu@outlook.com>
2019-09-25 22:01:56 +0800

    ..

--- src/hdl_graph_slam/debug/plotOdom.py
+++ src/hdl_graph_slam/debug/plotOdom.py
@@ -17,11 +17,14 @@ def main(argv):
 
 	print(points.shape)
 
-	plt.plot(points[:,0],points[:,1],'r',label='register_odom') 
+	plt.plot(points[:,0],points[:,1],'r',label='register_odom')
+	plt.plot(points[0,0],points[0,1],'r*')
 
 	plt.plot(points[:,3],points[:,4],'b',label='optimize_odom')
+	plt.plot(points[0,3],points[0,4],'b*')
 	
 	plt.plot(points[:,6],points[:,7],'k',label='gps_odom')
+	plt.plot(points[0,6],points[0,7],'k*')
 	
 	plt.axis('equal')
 	
